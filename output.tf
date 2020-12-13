2af984dbbced584d97b10c9ddbf441eea2fd4c8c
Vladlen Rodionov <vrodi18@gmail.com>
2020-12-13 20:22:07 +0000

    edit Outputs

--- output.tf
+++ output.tf
@@ -1,31 +1,31 @@
-output "vpc" {
-  value = "${module.vpc.vpc}"
+output "vpc_id" {
+  value = "${module.vpc.vpc_id}"
 }
 output "private_subnet1" {
-  value = "${module.vpc.private_subnets[0]}"
+  value = "${module.vpc.private_subnet1}"
 }
 
 
 output "private_subnet2" {
-  value = "${module.vpc.private_subnets[1]}"
+  value = "${module.vpc.private_subnet2}"
 }
 
 output "private_subnet3" {
-  value = "${module.vpc.private_subnets[2]}"
+  value = "${module.vpc.private_subnet3}"
 }
 
 
 output "public_subnet1" {
-  value = "${module.vpc.public_subnets[0]}"
+  value = "${module.vpc.public_subnet1}"
 }
 
 
 output "public_subnet2" {
-  value = "${module.vpc.public_subnets[1]}"
+  value = "${module.vpc.public_subnet2}"
 }
 
 output "public_subnet3" {
-  value = "${module.vpc.public_subnets[2]}"
+  value = "${module.vpc.public_subnet3}"
 }
 
 output "region" {
